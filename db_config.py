63db8a302f97921652eb989db91f983e373346fd
kuberan-35 <kuberankarthikeyan@gmail.com>
2025-07-25 00:13:31 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,5 +1,9 @@
-
 from sqlalchemy import create_engine
+import pandas as pd
 
-def get_engine():
-    return create_engine("postgresql://postgres:63693103k@@localhost:5432/ola_project")
+# Replace with your credentials
+engine = create_engine('postgresql://postgres:63693103k@localhost:5432/ola_project')
+
+def fetch_data(query):
+    with engine.connect() as conn:
+        return pd.read_sql(query, conn)
