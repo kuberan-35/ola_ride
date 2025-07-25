c045110963c1df6e3fbc7a6c78016a3419812b12
kuberan-35 <kuberankarthikeyan@gmail.com>
2025-07-25 19:08:18 +0530

    Update ola_app.py

--- ola_app.py
+++ ola_app.py
@@ -1,12 +1,28 @@
+
+import streamlit as st
+from sqlalchemy import create_engine
 import pandas as pd
-import psycopg2 as ps
-from sqlalchemy import create_engine 
 
-df = pd.read_csv('ola_data.csv')
+# Replace with your credentials
+engine = create_engine('postgresql://postgres:63693103k@localhost:5432/ola_project')
 
-engine = create_engine("postgresql+psycopg2://postgres:63693103k@@localhost:5432/ola_project")
+def fetch_data(query):
+    with engine.connect() as conn:
+        return pd.read_sql(query, conn)
 
-df.to_sql("ola_ride", engine, if_exists='replace', index=False)
+df = pd.read_csv("ola_rides.csv")
+
+
+total_rides = df.shape[0]
+total_customers = df['Customer_ID'].nunique()
+completed_rides = df[df['Booking_Status'] == 'Completed'].shape[0]
+cancellation_rate = round(
+    df[df['Booking_Status'] == 'Canceled'].shape[0] / total_rides * 100, 2
+)
+
+st.metric("Total Rides", total_rides)
+st.metric("Completed Rides", completed_rides)
+st.metric("Cancellation Rate", f"{cancellation_rate}%")
 
 
 
